ab792668dd778edbf8c68fd88c7fddc452a690d0
mishingo <miguel@localstaffingllc.com>
2017-01-15 01:55:08 -0500

    fix woah svg

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -1,4 +1,20 @@
 @extends('layouts.display')
 @section('content')
-hello
+<div class="hero">
+   <div class="container">
+      <div class="row center-align pa-m--s pt-l--s">
+         <div class="col s12 m6 offset-m3">
+            <div class="row">
+               <img src="{{asset('images/logo-icon.svg')}}" width="60">
+               <h1 class="white-text thin"> Thank You!</h1>
+               <hr />
+               <h4 class="white-text mt-m--s"> Our Gift to You </h4>
+            </div>
+            <div class="row pa-m--m pa-s--s white mt-xl--s z-depth-2">
+
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
 @endsection
